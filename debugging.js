3c209b047599f654446f085a355ed7789168e556
KirstenJackson <jac19019@byui.edu>
2023-09-16 18:06:00 -0600

    w01 task

--- debugging.js
+++ debugging.js
@@ -1,11 +1,3 @@
-// const PI = 3.14;
-// let radius = 3;
-// let area = 0;
-// area = radius * radius * PI;
-// console.log(area);
-// radius = 4;
-// area = radius * radius * PI;
-// console.log(area);
 
 const PI = 3.14;
 let area = 0;
