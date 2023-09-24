1d7e2494ff997562577ae0cbb86051ef08f19115
KirstenJackson <jac19019@byui.edu>
2023-09-23 18:00:04 -0600

    w02 work

---
+++ w02/js_operators.js
@@ -0,0 +1,38 @@
+// declare variables 
+let x = 10;
+let y = 3;
+
+let string1 = "yummy ";
+let string2 = "nom nom";
+
+// Add two numbers
+x + y
+
+// Multiply two numbers
+x * y
+
+// Subtract one from a number
+x - y
+
+// concatinate two strings together.
+string1 + string2
+
+// assign a value to a variable
+let z = 45;
+
+// increment the value in a variable by 3
+morez = z + 3;
+
+// compare two values to see if they are the same
+x == y;
+
+// check to see if one number is less than another number
+x < y;
+
+// Check to see if two values are NOT equal
+x != y;
+
+// check to see if a value is less than 10 and greater than 0
+(y < 10 && y > 0);
+
+const ward = `<strong>Home Ward</strong>: ${homeWard}`;
---
+++ w02/scripts/w02-task.js
@@ -0,0 +1,34 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Kirsten Jackson";
+let currentYear = "2023";
+let profilePicture = "images/me.png";
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+let yearElement = document.querySelector("#year");
+let imageElement = document.getElementById("picture");
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+document.getElementById(yearElement).textContent;
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile image of ${fullName}`);
+
+
+
+/* Step 5 - Array */
+const favoriteFoods = ["Chocolate", "Sushi", "Nachos"];
+
+let foodItem = "Soup";
+
+
+
+
+
